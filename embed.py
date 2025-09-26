909382640d7717b938f3881d0b2ae14ec5439c04
wbochenski <github@wbochenski.com>
2025-09-26 13:01:36 +0200

    embeddings.py -> embed.py, code refactor

---
+++ embed.py
@@ -0,0 +1,49 @@
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
+import pandas as pd
+import PyPDF2
+import os
+
+def get_paths_to_files(folder_path: str):
+    file_paths = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            full_path = os.path.join(root, file)
+            file_paths.append(full_path)
+    return file_paths
+
+def read_pdf(pdf_path: str) -> str:
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ""
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            text += page.extract_text()
+    return text
+
+def split_text_into_chunks(text, max_chunk_length):
+      return [text[i:i + max_chunk_length] for i in range(0, len(text), int(max_chunk_length/2))]
+
+def generate_embeddings(text, model):
+    embeddings = [model.encode(chunk).tolist() for chunk in text]
+    return embeddings
+
+def main(data_folder, model):
+    df = pd.DataFrame({'path': [], 'embedding': []})
+    files = get_paths_to_files(data_folder)
+
+    for path in tqdm(files):
+        content = read_pdf(path)
+        content = split_text_into_chunks(content, 256)
+        embeddings = generate_embeddings(content, model)
+
+        new_rows = [(path, embedding) for embedding in embeddings]
+        new_df = pd.DataFrame(new_rows, columns=['path', 'embedding'])
+
+        df = pd.concat([df, new_df], ignore_index=True)
+
+    df.to_csv(f'{data_folder}.csv', sep=";", index=False)
+
+if __name__ == "__main__":
+    model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+    main("data", model)
